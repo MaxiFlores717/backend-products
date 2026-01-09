70f805dc7e6ae381f3753cbcc694cb6f37bd8751
MaxiFlores717 <103212606+MaxiFlores717@users.noreply.github.com>
2026-01-09 19:44:47 -0300

    first commit

---
+++ src/main/java/com/example/springboot/backend/backend_products/repositories/ProductRepository.java
@@ -0,0 +1,11 @@
+package com.example.springboot.backend.backend_products.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import com.example.springboot.backend.backend_products.entities.Product;
+
+@RepositoryRestResource(path = "products")
+public interface ProductRepository extends CrudRepository<Product, Long>{
+
+}
