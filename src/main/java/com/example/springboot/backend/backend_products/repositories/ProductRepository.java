98eadbf999b83d6625ac7e1b069355c3f5827c72
MaxiFlores717 <103212606+MaxiFlores717@users.noreply.github.com>
2026-02-03 18:50:33 -0300

    add cors

--- src/main/java/com/example/springboot/backend/backend_products/repositories/ProductRepository.java
+++ src/main/java/com/example/springboot/backend/backend_products/repositories/ProductRepository.java
@@ -2,10 +2,12 @@ package com.example.springboot.backend.backend_products.repositories;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.example.springboot.backend.backend_products.entities.Product;
 
 @RepositoryRestResource(path = "products")
+@CrossOrigin(origins = "http://localhost:5173")
 public interface ProductRepository extends CrudRepository<Product, Long>{
 
 }
